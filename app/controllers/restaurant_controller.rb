402d0b7d2e4e5ce52393aaf0c4313a5e30da16c5
Adriana Castaneda <adriana.lcs316@gmail.com>
2016-03-22 16:36:18 -0500

    add pseudo search

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,7 +1,30 @@
 class RestaurantController < ApplicationController
   def index
     client = SODA::Client.new({:domain => "data.cityofchicago.org", :app_token => ENV['RESTAURANT_API_TOKEN']})
-    p client.get("4ijn-s7e5.json")
+    p client.get("4ijn-s7e5.json",{"$limit" => 10, :aka_name=>"NICKY'S RESTAURANT"})
+  end
+
+  def search
+
   end
 end
 # https://data.cityofchicago.org/resource/4ijn-s7e5.json
+
+# API Fields
+# Inspection ID inspection_id
+# DBA Name dba_name
+# AKA Name aka_name
+# License # license_
+# Facility Type facility_type
+# Risk risk
+# Address address
+# City city
+# State state
+# Zip zip
+# Inspection Date inspection_date
+# Inspection Type inspection_type
+# Results results
+# Violations violations
+# Latitude latitude
+# Longitude longitude
+# Location location
